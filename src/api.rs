95f64e9ba89f7115ae9ef7f95255b6ca061b0ef6
almafa64 <bhatalyak@gmail.com>
2025-09-21 20:32:29 +0200

    timeout on api requests + global client

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,6 @@
-use reqwest::{Response, Result};
+use std::{sync::LazyLock, time::Duration};
+
+use reqwest::{Client, Response, Result};
 
 #[derive(Debug, Clone, serde::Deserialize)]
 pub struct ZipFile {
@@ -27,23 +29,31 @@ pub type Mods = Vec<Mod>;
 // TODO: try http and if doesnt work try https
 const HTTP_TYPE: &str = "https://";
 
+static CLIENT: LazyLock<Client> = LazyLock::new(|| {
+    Client::builder().timeout(Duration::from_secs(10)).build().unwrap()
+});
+
+fn get_client() -> &'static Client {
+    &CLIENT
+}
+
 pub async fn website_exists(api_address: &str) -> Result<bool> {
 	let path = format!("{}{}/mods", HTTP_TYPE, api_address);
-	let res = reqwest::get(path).await?;
+	let res = get_client().get(path).send().await?;
 
 	Ok(res.status().is_success())
 }
 
 pub async fn get_branch_names(api_address: &str) -> Result<BranchNames> {
 	let path = format!("{}{}/mods", HTTP_TYPE, api_address);
-	let res = reqwest::get(path).await?.json::<BranchNames>().await?;
+	let res = get_client().get(path).send().await?.json::<BranchNames>().await?;
 
 	Ok(res)
 }
 
 pub async fn get_mods_in_branch(api_address: &str, branch_name: &str) -> Result<BranchInfo> {
 	let path = format!("{}{}/mods/{}", HTTP_TYPE, api_address, branch_name);
-	let res = reqwest::get(path).await?.json::<BranchInfo>().await?;
+	let res = get_client().get(path).send().await?.json::<BranchInfo>().await?;
 
 	Ok(res)
 }
@@ -57,14 +67,14 @@ pub async fn request_mod(
 		"{}{}/mods/{}/{}",
 		HTTP_TYPE, main_address, branch_name, file_name
 	);
-	let res = reqwest::get(path).await?;
+	let res = get_client().get(path).send().await?;
 
 	Ok(res)
 }
 
 pub async fn request_mod_zip(main_address: &str, branch_name: &str) -> Result<Response> {
 	let path = format!("{}{}/mods/{}", HTTP_TYPE, main_address, branch_name);
-	let res = reqwest::get(path).await?;
+	let res = get_client().get(path).send().await?;
 
 	Ok(res)
 }
